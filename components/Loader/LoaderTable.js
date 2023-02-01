27880fdf27f4674a2f289b5ecccbddfd8a2d6579
gokulkrishh <gokulkrishh>
2023-02-01 20:40:11 +0530

    Fix loader

--- components/Loader/LoaderTable.js
+++ components/Loader/LoaderTable.js
@@ -1,13 +1,13 @@
 import { range } from 'utils/array';
 
-export default function LoaderTable({ tr = 4, td = 6 }) {
+export default function LoaderTable({ tr = 4, td = 5 }) {
 	return (
 		<>
 			{range(1, tr).map((num, index) => (
-				<tr className="border-b border-gray-200 last:border-0 hover:bg-gray-50" key={`${num}-${index}`}>
+				<tr className="animate-pulse border-b border-gray-200 last:border-0 hover:bg-gray-50" key={`${num}-${index}`}>
 					{range(1, td).map((tdNum, tdIndex) => (
 						<td key={`${tdNum}-${tdIndex}`} className="mt-1 h-[53px] pl-7">
-							<span className="block w-[60%] animate-pulse rounded-md bg-gray-200 p-[6px] " />
+							<span className="block w-[50%] rounded-md bg-gray-200 p-[6px]" />
 						</td>
 					))}
 				</tr>
