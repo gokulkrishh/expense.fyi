964af7b91b761473b085b2cd87fa410684c09942
gokulkrishh <gokulkrishh>
2023-02-15 16:39:16 +0530

    Fix reponsive issue for table actions

--- components/Table/TableHeaderActions.jsx
+++ components/Table/TableHeaderActions.jsx
@@ -7,9 +7,9 @@ import TableFilter from './TableFilter';
 
 export default function TableHeaderActions({ isPremiumPlan, filterKey, title, isLoading, showFilter, onFilterChange }) {
 	return (
-		<div className="mb-4 flex h-[34px] w-full items-center justify-between">
+		<div className="mb-12 flex h-[34px] w-full flex-col xs:mb-4 xs:flex-row xs:items-center xs:justify-between">
 			<h3 className="text-black">Data</h3>
-			<div className="flex justify-end">
+			<div className="mt-2 flex justify-end xs:mt-0">
 				{showFilter && !isLoading ? <TableFilter filterKey={filterKey} onFilterChange={onFilterChange} /> : null}
 				{isPremiumPlan && !isLoading ? (
 					<ExportButton filename={`${title}-${format(new Date(), dateFormatStr)}.csv`} />
