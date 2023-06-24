90f31574e1ef519157e3cb6b148bf9e44427973f
gokulkrishh <2944237+gokulkrishh@users.noreply.github.com>
2023-06-24 12:53:34 +0530

    Initial commit to re-write

--- pages/api/unauthorized.js
+++
@@ -1,3 +0,0 @@
-export default function handle(_, res) {
-	res.status(401).json({ message: 'Unauthorized request' });
-}
--- prisma/functions/functions.sql
+++
@@ -1,5 +0,0 @@
-begin
-  insert into public.users (id, email)
-  values (new.id, new.email);
-  return new;
-end;
