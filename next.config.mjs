7f2aa9df9031b132dc1db40e87784e48ce79794c
gokulkrishh <2944237+gokulkrishh@users.noreply.github.com>
2023-07-13 08:37:55 +0530

    fix manifest loading issue

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
 };
 
 const ContentSecurityPolicy = `
-    default-src 'self';
+    default-src 'self' expense.fyi;
     script-src 'self' 'unsafe-eval' 'unsafe-inline' app.lemonsqueezy.com *.cloudfront.net assets.lemonsqueezy.com *.googletagmanager.com;
     child-src 'self' expensefyi.lemonsqueezy.com;
     style-src 'self' 'unsafe-inline';
