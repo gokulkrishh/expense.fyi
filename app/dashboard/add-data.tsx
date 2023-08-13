d79d59606c381e4172a3483790e64d6f4011250c
gokulkrishh <2944237+gokulkrishh@users.noreply.github.com>
2023-08-13 17:04:03 +0530

    Support autocomplete in overview page for add expense

--- app/dashboard/add-data.tsx
+++ app/dashboard/add-data.tsx
@@ -1,12 +1,17 @@
 'use client';
 
+import { useCallback } from 'react';
+
 import Add from 'components/add-button';
 import { useOverview } from 'components/context/overview-provider';
+
+import { lookup } from 'lib/lookup';
 
 const AddData = () => {
 	const { data } = useOverview();
 	const { mutateExpenses } = data.mutate;
-	return <Add type="expenses" mutate={mutateExpenses} />;
+	const onLookupExpenses = useCallback((name: string) => lookup({ data: data.expenses, name }), [data]);
+	return <Add type="expenses" mutate={mutateExpenses} onLookup={onLookupExpenses} />;
 };
 
 export default AddData;
