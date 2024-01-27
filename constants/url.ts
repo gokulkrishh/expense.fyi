97801e5215a54741ec5146376b52785743f88ef9
Deiver Julio <deiver-jc@live.com>
2024-01-27 15:46:24 -0500

    update domain url

--- constants/url.ts
+++ constants/url.ts
@@ -3,7 +3,7 @@ import { views } from './table';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-const domain = process.env.NEXT_PUBLIC_SITE_URL;
+const domain = 'gastos.site';
 const local = 'localhost:3000';
 const home = isProduction ? domain : local;
 
